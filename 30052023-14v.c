50e6992706b80a7abcf67585945c67441ed1d76f
JOVANYPZP <132587920+JOVANYPZP@users.noreply.github.com>
2023-05-30 10:07:04 -0600

    Add files via upload

---
+++ 30052023-14v.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int ventas = 5000;
+    int años = 8;
+    int sal_min = 100;
+    int IVA = 15;
+
+    if (ventas < (sal_min * 3) && (años >> (10 * IVA)))
+    {
+        printf("La condición es verdadera");
+    }
+    else
+    {
+        printf("La condición es falsa");
+    }
+
+    return 0;
+}
